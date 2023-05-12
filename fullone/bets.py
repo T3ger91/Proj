15015f7fcab9545defec87d4656967cdc2e1edb2
T3ger91 <63410156+T3ger91@users.noreply.github.com>
2023-05-13 00:44:06 +0200

    Update bets.py

--- fullone/bets.py
+++ fullone/bets.py
@@ -1,3 +1,4 @@
+#importing 4 files in one to simplyfise structure and make it more logical 
 from Inside_bets import Inside_bet
 from Outside_bets import Outside_bet
 from Random_bet import Random_bet
