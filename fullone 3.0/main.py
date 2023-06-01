1daef7b6c4259818fd6d6bdfe0b8ee1b148777fd
Gleb Laskov <63410156+T3ger91@users.noreply.github.com>
2023-06-01 10:17:12 +0200

    Update main.py

--- fullone 3.0/main.py
+++ fullone 3.0/main.py
@@ -7,7 +7,7 @@ import PySimpleGUI as sg
 import webbrowser
 from slow_print import slow_print
 slow_print(
-    "As usual, you returned late in the evening from the casino, having lost everything once again. When you entered your house, you were struck from behind at the door. You woke up in a dark room filled with slot machines, facing a dark silhouette of a masked man with a weapon pointed at you. Suddenly, a voice came over the intercom, saying, ""Hey, let's play a game, buddy. You got 1000 dinars in your wallet. If you wanna get the hell outta here, you gotta win 100,000 dinars from four games we got for ya. Inside of this facility, you have a dealer, your judge, jury, and an executioner, who you can challenge. I wouldn't try anything else, or you will face the consequences! Good luck. And if you lose..."
+    "As usual, you returned from the casino, you just lost everything once again. You came home,and fall on the flor.You woke up in a dark room filled with slot machines, facing a dark silhouette of masked men, weapon was directed to you. Suddenly, a voice came over the intercom, saying, ""Hey, let's play a game, buddy. You got 1000 dinars in your wallet. If you wanna get the hell outta here, you gotta win 100,000 dinars from four games we got for ya. Inside of this facility, you have a dealer, your judge, jury, and an executioner, who you can challenge. I wouldn't try anything else, or you will face the consequences! Good luck. And if you lose..."
 )
 slow_print("1000 this is ur money now\nto escape this asshole place u need to gain 100.000 dinars")
 from MenuS import Menu
